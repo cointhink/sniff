5aaf250e11f4ac1bdaa25f7863b849d0621190fe
Don Park <donp@donp.org>
2025-06-09 08:30:21 +0700

    timer extract

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use reqwest_websocket::Message;
 
 mod config;
 mod rpc;
+mod timer;
 mod ws;
 
 #[tokio::main]
@@ -12,42 +13,31 @@ async fn main() {
     log::info!("scoop loaded {}", config::path(config::FILENAME));
 
     let (mut tx, mut rx) = ws::connect(&config.geth_url).await.unwrap();
-    let rpc_subscribe = rpc::RpcCall::new(
-        "eth_subscribe",
-        vec![
-            serde_json::Value::String("newPendingTransactions".into()),
-            serde_json::Value::Bool(true),
-        ],
-    );
     let sender = async move {
+        let rpc_subscribe = rpc::RpcCall::new(
+            "eth_subscribe",
+            vec![
+                serde_json::Value::String("newPendingTransactions".into()),
+                serde_json::Value::Bool(true),
+            ],
+        );
         let cmd = serde_json::to_string(&rpc_subscribe).unwrap();
         log::info!("{}", cmd);
         tx.send(Message::Text(cmd)).await.unwrap();
     };
 
     let reader = async move {
-        let mut rx_byte_count: u128 = 0;
-        let mut rx_msg_count: u128 = 0;
-        let mut now = std::time::Instant::now();
+        let mut timer = timer::new();
+
         while let Some(message) = rx.try_next().await.unwrap() {
             if let Message::Text(text) = message {
-                rx_byte_count += text.len() as u128;
-                rx_msg_count += 1;
+                timer.next_msg(text.len());
                 // println!("{}", text);
             }
-            let duration = now.elapsed();
-            let duration_ms10 = now.elapsed().as_millis() + 1;
-            log::info!(
-                "elapsed {:?}. {:?} msg/sec. {:?} kbytes/sec",
-                duration,
-                (rx_msg_count * 1000).div_ceil(duration_ms10) as f64 / 10.0,
-                (rx_byte_count).div_ceil(duration_ms10) as f64 / 10.0
-            );
-            if duration.as_secs() > 10 {
-                now = std::time::Instant::now();
-                rx_byte_count = 0;
-            }
+            timer.report();
+            timer.reset_after_seconds(10);
         }
     };
+
     futures_util::future::join(sender, reader).await;
 }
---
+++ src/timer.rs
@@ -0,0 +1,40 @@
+use std::time;
+
+pub struct Timer {
+    rx_byte_count: u128,
+    rx_msg_count: u128,
+    now: time::Instant,
+}
+
+pub fn new() -> Timer {
+    Timer {
+        rx_byte_count: 0,
+        rx_msg_count: 0,
+        now: time::Instant::now(),
+    }
+}
+
+impl Timer {
+    pub fn next_msg(self: &mut Self, msg_len: usize) {
+        self.rx_byte_count += msg_len as u128;
+        self.rx_msg_count += 1;
+    }
+
+    pub fn report(self: &mut Self) {
+        let duration = self.now.elapsed();
+        let duration_ms10 = self.now.elapsed().as_millis() + 1;
+        log::info!(
+            "elapsed {:?}. {:?} msg/sec. {:?} kbytes/sec",
+            duration,
+            (self.rx_msg_count * 1000).div_ceil(duration_ms10) as f64 / 10.0,
+            (self.rx_byte_count).div_ceil(duration_ms10) as f64 / 10.0
+        );
+    }
+    pub fn reset_after_seconds(self: &mut Self, seconds: usize) {
+        let duration = self.now.elapsed();
+        if duration.as_secs() > seconds {
+            self.now = std::time::Instant::now();
+            self.rx_byte_count = 0;
+        }
+    }
+}
